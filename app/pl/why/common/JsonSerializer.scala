a23521d9d0eb3825b43206bb8cf92e8c360ccd0a
Bartłomiej Pieczka <bartlomiej.pieczka@gmail.com>
2017-06-24 10:25:47 +0200

    update to akka 2.5.3

--- app/pl/why/common/JsonSerializer.scala
+++ app/pl/why/common/JsonSerializer.scala
@@ -6,21 +6,22 @@ import org.json4s.native.Serialization
 import org.json4s.native.Serialization.{read, write}
 import org.json4s.ext.EnumNameSerializer
 
-class JsonSerializer extends SerializerWithStringManifest{
+class JsonSerializer extends SerializerWithStringManifest {
   implicit val formats = Serialization.formats(NoTypeHints)
 
-  def toBinary(o:AnyRef):Array[Byte] = {
+  def toBinary(o: AnyRef): Array[Byte] = {
     val json = write(o)
     json.getBytes()
   }
 
-  def fromBinary(bytes:Array[Byte], manifest:String):AnyRef = {
+  def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = {
     val m = Manifest.classType[AnyRef](Class.forName(manifest))
     val json = new String(bytes, "utf8")
     read[AnyRef](json)(formats, m)
   }
 
-  def identifier:Int = 999
-  def manifest(o:AnyRef):String = o.getClass.getName
+  def identifier: Int = 999
+
+  def manifest(o: AnyRef): String = o.getClass.getName
 }
 
